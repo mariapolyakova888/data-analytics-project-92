769e4f4d8cad1cc4c9b7276e643746037565fd08
Мария <128679585+mariapolyakova888@users.noreply.github.com>
2023-06-22 11:28:11 +0700

    Add 3 new files and updated 1 file

--- queries.sql
+++ queries.sql
@@ -1,17 +1,18 @@
 /* 4-й шаг проекта:
  Подсчет общего количества покупателей */
 
-select COUNT(customer_id) as customers_count
+select
+    count(customer_id) as customers_count
 from customers;
 
-/* Запросы для 5-го шага проекта:
+/* 5-й шаг проекта:
 
-1. Отчет о десятке лучших продавцов, выполнивших наибольшую выручку.
-Отсортирован по убыванию выручки */
+1. Отчет о десятке лучших продавцов, выполнивших наибольшую выручку.Отсортирован по убыванию выручки */
 
-select CONCAT(e.first_name, ' ', null, e.last_name) as name,
-       COUNT(s.sales_id) as operations, 
-       SUM(s.quantity * p.price) as income
+select
+    concat(e.first_name, ' ', null, e.last_name) as name,
+    count(s.sales_id) as operations, 
+    sum(s.quantity * p.price) as income
 from sales s
 left join employees e
 on s.sales_person_id = e.employee_id
@@ -26,18 +27,20 @@ limit 10
 Отсортирован по возрастанию выручки */
 
 with tab as (
-    select CONCAT(e.first_name, ' ', null, e.last_name) as name,
-           round(AVG(s.quantity*p.price), 0) as average_income,
-           round(AVG(s.quantity * p.price), 0) as avg_income_all_employees
+    select
+        concat(e.first_name, ' ', null, e.last_name) as name,
+        round(avg(s.quantity*p.price), 0) as average_income,
+        round(avg(s.quantity * p.price), 0) as avg_income_all_employees
     from sales s
     left join employees e
     on s.sales_person_id = e.employee_id
     left join products p
     on s.product_id = p.product_id
     group by 1
-    )
-    
-select name, average_income
+)  
+select
+    name,
+    average_income
 from tab
 group by 1, 2, avg_income_all_employees
 having 2 < avg_income_all_employees
@@ -48,10 +51,11 @@ limit 10
 /* 3. Отчет о выручке по дням недели, отсортированный по порядковому номеру дня недели */
 
 with tab as (
-    select to_char(s.sale_date, 'id') as num_weekday,
-           to_char(date(s.sale_date), 'day') as weekday,
-           CONCAT(e.first_name, ' ', null, e.last_name) as name,
-           round(AVG(s.quantity*p.price), 0) as income
+    select
+        to_char(s.sale_date, 'id') as num_weekday,
+        to_char(date(s.sale_date), 'day') as weekday,
+        concat(e.first_name, ' ', null, e.last_name) as name,
+        round(avg(s.quantity*p.price), 0) as income
     from sales s
     left join employees e
     on s.sales_person_id = e.employee_id
@@ -59,12 +63,66 @@ with tab as (
     on s.product_id = p.product_id
     group by 1, 2, 3
     order by 1, 3
-    )
-
-select tab.name,
-       tab.weekday,
-       income
+)
+select
+    tab.name,
+    tab.weekday,
+    income
 from tab
 group by tab.name, tab.weekday, tab.num_weekday, income
 order by tab.name, tab.num_weekday asc
+;
+
+/* 6-й шаг проекта 
+  
+  1. Отчет о количестве покупателей в разных возрастных группах */
+
+select
+    case
+        when age >= 16 and age <= 25 then '16-25'
+    	when age >= 26 and age <= 40 then '26-40'
+    	when age >= 41 then '40+'
+    end as age_category,
+    count(customer_id) as count
+from customers
+group by 1
+order by 1
+;
+
+/* 2. Количество уникальных покупателей и выручка, которую они принесли */
+
+select distinct to_char(s.sale_date, 'YYYY-MM') as date,
+       count(distinct s.customer_id) as total_customers,
+       sum(s.quantity * p.price) as income
+from sales s
+inner join products p
+on s.product_id = p.product_id
+group by date
+;
+
+/* 3. Отчет о покупателях, 1-ая покупка которых совершена в ходе проведения акций (акционные товары равны 0) */
+
+with tab as (
+    select distinct
+        c.customer_id,
+        concat(c.first_name, ' ', null, c.last_name) as customer,
+        s.sale_date,
+        concat(e.first_name, ' ', null, e.last_name) as seller,
+        p.price
+from sales s
+inner join customers c
+on s.customer_id = c.customer_id
+inner join employees e
+on s.sales_person_id = e.employee_id
+inner join products p
+on s.product_id = p.product_id
+where price = 0
+order by c.customer_id
+)
+select distinct on (tab.customer)
+    tab.customer,
+    tab.sale_date,
+    tab.seller
+from tab
+order by tab.customer
 ;
